406370979b4cfeaf1808039fedf90611e9ea1117
Vilhelm von Ehrenheim <vilhelm.ehrenheim@klarna.com>
2016-05-30 20:22:05 +0200

    Updated documentation.

--- R/binR.r
+++ R/binR.r
@@ -11,8 +11,10 @@
 #' @export
 ##------------------------------------------------------------------------------
 binR <- function(fx, data, algorithm=c("quantile", "rpart", "manual"), ...) {
+  # Extract additional arguments
   dots <- list(...)
 
+  # Match algorithm and run the respective function
   out <- switch(match.arg(algorithm, c("quantile", "rpart", "manual")),
                 quantile=do.call(binR_quantile, c(list(fx), list(data), dots)),
                 rbind=do.call(binR_rbind, c(list(fx), list(data), dots)),
